302b816cebfe97d0d96f952dac39e8ae96e46dbc
sarathlass <sarathninja1995@gmail.com>
2024-06-03 19:00:09 +0530

    Launching a browser

---
+++ Selenium.java
@@ -0,0 +1,15 @@
+public class automation
+  {
+
+public static void main(String[]args)
+    {
+
+      WebDriver d = new ChromeDriver();
+      d.get("https://www.facebook.com/");
+      d.manage.windows.maximize;
+      
+
+    }
+
+
+  }
